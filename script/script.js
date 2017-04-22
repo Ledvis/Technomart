5da9f322a499ba8f8877199df1277b2e61616847
Eugene <fedyai.eugen@gmail.com>
2017-04-22 16:42:46 +0300

    v 5.0 add js

--- script/script.js
+++ script/script.js
@@ -2,17 +2,36 @@ var feedback = document.querySelector(".feedback");
 var popup = document.querySelector(".modal-content");
 var overlay = document.querySelector(".modal-overlay")
 var close = popup.querySelector(".close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("#name");
+var storage = localStorage.getItem("name");
 
 feedback.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.add("modal-content-show");
 	overlay.classList.add("modal-overlay-show");
+	if (storage) {
+		storage = name.value;
+		email.focus();
+	} else {
+		name.focus();
+	}
 });
 
 close.addEventListener("click", function (event) {
 	event.preventDefault();
 	popup.classList.remove("modal-content-show");
 	overlay.classList.remove("modal-overlay-show");
+	popup.classList.remove("modal-content-error");
+});
+
+form.addEventListener("submit", function (event) {
+	if (!name.value) {
+		event.preventDefault();
+		popup.classList.add("modal-content-error");
+	} else {
+		localStorage.setItem("name", "name.value");
+	}
 });
 
 window.addEventListener("keydown", function (event) {
@@ -20,6 +39,7 @@ window.addEventListener("keydown", function (event) {
 		if (popup.classList.contains("modal-content-show")) {
 			popup.classList.remove("modal-content-show");
 			overlay.classList.remove("modal-overlay-show");
+			popup.classList.remove("modal-content-error");
 		}
 	}
 });
@@ -47,4 +67,41 @@ window.addEventListener("keydown", function (event) {
 			overlay.classList.remove("modal-overlay-show");
 		}
 	}
+});
+
+var deliveryLi = document.querySelector(".delivery-item");
+var garantyLi = document.querySelector(".garanty-item");
+var creditLi = document.querySelector(".credit-item");
+var deliveryDiv = document.querySelector(".delivery");
+var garantyDiv = document.querySelector(".garanty");
+var creditDiv = document.querySelector(".credit");
+
+deliveryLi.addEventListener("click", function (event) {
+	event.preventDefault;
+	deliveryLi.classList.add("active-item");
+	garantyLi.classList.remove("active-item");
+	creditLi.classList.remove("active-item");
+	deliveryDiv.classList.add("show");
+	garantyDiv.classList.remove("show");
+	creditDiv.classList.remove("show");
+});
+
+garantyLi.addEventListener("click", function (event) {
+	event.preventDefault;
+	garantyLi.classList.add("active-item");
+	deliveryLi.classList.remove("active-item");
+	creditLi.classList.remove("active-item");
+	garantyDiv.classList.add("show");
+	deliveryDiv.classList.remove("show");
+	creditDiv.classList.remove("show");
+});
+
+creditLi.addEventListener("click", function (event) {
+	event.preventDefault;
+	creditLi.classList.add("active-item");
+	deliveryLi.classList.remove("active-item");
+	garantyLi.classList.remove("active-item");
+	creditDiv.classList.add("show");
+	deliveryDiv.classList.remove("show");
+	garantyDiv.classList.remove("show");
 });
